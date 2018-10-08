1c3f6a61713fe7199331e4dc37d9696d57eefa9e
RVAZQUEZLO <RVAZQUEZLO@112SALAS06>
2018-10-08 14:57:37 -0500

    Conexion y empezar MainWindow

---
+++ ProyectoEcologia/Conexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProyectoEcologia
+{
+    class Conexion
+    {
+        public static SqlConnection agregarConexion()
+        {
+            SqlConnection conexion;
+
+            try
+            {
+                conexion = new SqlConnection("Copy paste la direccion de base de datos");
+                conexion.Open();
+                MessageBox.Show("Conectado");//Quitar despues de pruebas
+            }
+            catch (Exception e)
+            {
+                conexion = null;
+                MessageBox.Show("no conectado" + e);//Quitar despues de pruebas
+            }
+            return conexion;
+        }
+    }
+}
--- ProyectoEcologia/MainWindow.xaml.cs
+++ ProyectoEcologia/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,37 @@ namespace ProyectoEcologia
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbUsuario.Focus();
+        }
+
+        private void btAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conexion;
+            SqlCommand comando;
+            SqlDataReader lector;
+
+            try
+            {
+                conexion = Conexion.agregarConexion();
+                comando = new SqlCommand(String.Format("select contrasena from Usuario where idUsuario = '{0}'",
+                    tbUsuario.Text), conexion);
+                lector = comando.ExecuteReader();
+                if (lector.Read()) //hubo una contrasena para el usuario ingresado
+                {
+                    if (lector.GetString(0).Equals(pboxContrasena.Password.ToString()))
+                    {
+
+                    }
+                }
+            }catch(Exception ex)
+            {
+
+            }
+            
         }
     }
 }
